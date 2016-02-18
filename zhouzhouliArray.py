eba7ac9fde3ce6ff21522c945f89c8515a762bea
toto233 <toto233@gmail.com>
2016-02-18 15:15:36 +0800

    修改注释
    修改注释

--- zhouzhouliArray.py
+++ zhouzhouliArray.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 05 18:15:33 2015
-
-@author: Administrator
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Fri Dec 04 00:35:24 2015
 
-@author: xub
+@author: toto233
 """
 
 from splinter.browser import Browser
@@ -18,13 +11,13 @@ from time import sleep
 #用户名，密码
 username = range(10)
 passwd = range(10)
-#分神
+#user1
 username[0] = "qqnum1"
 passwd[0] = "pw1"
-#飞雪映画
+#user2
 username[1]= "qqnum2"
 passwd[1] = "pw2"
-#叽里咕噜打呼噜
+#user3
 username[2]="qqnum3"
 passwd[2]="pw3"
 
@@ -34,27 +27,29 @@ gift_url = "http://wuxia.qq.com/act/a20150914wxzzl/index.html"
 def login():
     global b
     b = Browser(driver_name="chrome")
-    for i in range(0,7):
+    for i in range(0,3):
         b.visit(gift_url)
         b.find_by_id("ptLoginBtn").click()
-        sleep(3)
+        sleep(1)
         with b.get_iframe('loginFrame') as iframe:
             iframe.find_by_id('u').fill(username[i])
             iframe.find_by_id('p').fill(passwd[i])
             iframe.find_by_id('go').click()
-        sleep(3)
+        sleep(1)
         b.find_by_tag("a")[2].click()
-        sleep(4)
+        sleep(1)
+        #大区，需要改成自己的，这是大地飞鹰
         b.find_by_xpath('//select[@id="area1ContentId_wuxia"]/option[@value="7609516"]')._element.click()
-        sleep(3)
+        sleep(1)
+        #服务器，需要改成自己的，这是藏锋谷
         b.find_by_xpath('//select[@id="areaContentId_wuxia"]/option[@value="2002"]')._element.click()
-        sleep(3)
+        sleep(1)
         b.find_by_id("confirmButtonId_wuxia").click()
-        sleep(3)
+        sleep(1)
         b.get_alert().dismiss()
         sleep(1)
         b.find_by_id("ptLogoutBtn").click()
-        sleep(10)
+        sleep(5)
     print u"领取完毕"
     sleep(3)
 
